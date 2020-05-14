b55a87d79b1127dedb476d7fe15d1a000a05f980
Justin Schneck <jschneck@mac.com>
2020-05-14 09:32:56 -0400

    Update phoenix to test v1.5.1

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule PhoenixClient.Mixfile do
     [
       {:websocket_client, "~> 1.3"},
       {:jason, "~> 1.0", optional: true},
-      {:phoenix, github: "phoenixframework/phoenix", tag: "v1.4.11", only: :test},
+      {:phoenix, github: "phoenixframework/phoenix", tag: "v1.5.1", only: :test},
       {:plug_cowboy, "~> 2.0", only: :test},
       {:ex_doc, "~> 0.18", only: :dev}
     ]
